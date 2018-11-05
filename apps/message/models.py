8586f26439127caf94d969b368f875b25b84984a
stuartnankai <jians@kth.se>
2018-11-05 10:47:17 +0100

    Add css change the model's verbose_name

--- apps/message/models.py
+++ apps/message/models.py
@@ -8,16 +8,16 @@ from django.db import models
 
 # 继承于django.db.models.Model
 class UserMessage(models.Model):
-    object_id = models.CharField(primary_key=True, max_length=50,default="", verbose_name="主键")
+    object_id = models.CharField(primary_key=True, max_length=50,default="", verbose_name="Primary key")
     # 设置最大长度，verbose_name在后台显示字段会用到
-    name = models.CharField(max_length=20,null=True,blank=True,default="", verbose_name=u"用户名")
+    name = models.CharField(max_length=20,null=True,blank=True,default="", verbose_name=u"Username")
     # Django提供内置的邮箱字段会帮忙验证` default_validators = [validators.validate_email]`
-    email = models.EmailField(verbose_name=u"邮箱")
-    address = models.CharField(max_length=100, verbose_name=u"联系地址")
-    message = models.CharField(max_length=500, verbose_name=u"留言信息")
+    email = models.EmailField(verbose_name=u"Email")
+    address = models.CharField(max_length=100, verbose_name=u"Address")
+    message = models.CharField(max_length=500, verbose_name=u"Message")
 
     class Meta:
-        verbose_name = u"用户留言信息"
+        verbose_name = u"Message"
 
         # 指明复数信息，否则后台显示"用户留言s"
         verbose_name_plural = verbose_name
